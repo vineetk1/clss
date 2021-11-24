91088e5227158375cc2f9e9430eb1111b4291de7
vineetk1 <vineetchandhok@gmail.com>
2021-11-24 11:26:38 -0500

    rewrite of load-from-checkpoint and resume-from-checkpoint

--- Main.py
+++ Main.py
@@ -24,7 +24,7 @@ def main():
     # last file name in command-line has dictionaries of parameters
     params_file_path = argv[len(argv) - 1]
     with open(params_file_path, 'r') as paramF:
-        user_params = [
+        user_dicts = [
             dictionary for line in paramF if line[0] == '{'
             and isinstance(dictionary := literal_eval(line), dict)
         ]
@@ -32,87 +32,83 @@ def main():
         'misc', 'optz_sched', 'data', 'trainer', 'model_init',
         'ld_resume_chkpt'
     ]
-    if len(user_params) != len(user_dicts_keys):
+    if len(user_dicts) != len(user_dicts_keys):
         strng = (f'{argv[1]} MUST have {len(user_dicts_keys)} '
                  f'dictionaries even if the dictionaries are empty.')
         logg.critical(strng)
         exit()
-    user_params = {k: v for k, v in zip(user_dicts_keys, user_params)}
-    if 'ld_chkpt' in user_params[
-            'ld_resume_chkpt'] and 'resume_chkpt' in user_params[
+    user_dicts = {k: v for k, v in zip(user_dicts_keys, user_dicts)}
+    if 'ld_chkpt' in user_dicts[
+            'ld_resume_chkpt'] and 'resume_chkpt' in user_dicts[
                 'ld_resume_chkpt']:
         logg.critical('Cannot load- and resume-checkpoint at the same time')
         exit()
 
     seed_everything(63)
 
-    if 'ld_chkpt' in user_params['ld_resume_chkpt'] and user_params[
+    if 'ld_chkpt' in user_dicts['ld_resume_chkpt'] and user_dicts[
             'ld_resume_chkpt']['ld_chkpt'] is not None:
         model = Model.load_from_checkpoint(
-            checkpoint_path=user_params['ld_resume_chkpt']['ld_chkpt'])
-        dirPath = Path(user_params['ld_resume_chkpt']['ld_chkpt']).parents[1]
-        chkpt_params = full_load(
+            checkpoint_path=user_dicts['ld_resume_chkpt']['ld_chkpt'])
+        dirPath = Path(user_dicts['ld_resume_chkpt']['ld_chkpt']).parents[1]
+        chkpt_dicts = full_load(
             dirPath.joinpath('hyperparameters_used.yaml').read_text())
-        # chkpt_params has 2 additional dicts - app_specific_init, app_specific
-        assert len(user_params) == len(chkpt_params) - 2
+        # chkpt_dicts has 2 additional dicts - app_specific_init, app_specific
+        assert len(user_dicts) == len(chkpt_dicts) - 2
         # override  some user_dicts with chkpt_dicts
         for user_dict_k in user_dicts_keys:
-            if not user_params[user_dict_k] or user_dict_k == 'model_init':
-                user_params[user_dict_k] = chkpt_params[user_dict_k]
-        user_params['app_specific_init'] = chkpt_params['app_specific_init']
-        user_params['app_specific'] = chkpt_params['app_specific']
-        model.params(user_params['optz_sched'], user_params['app_specific'])
-    elif 'resume_from_checkpoint' in user_params[
-            'ld_resume_chkpt'] and user_params['ld_resume_chkpt'][
+            if (not user_dicts[user_dict_k] and
+                    user_dict_k != 'ld_resume_chkpt') or\
+                    user_dict_k == 'model_init':
+                user_dicts[user_dict_k] = chkpt_dicts[user_dict_k]
+        user_dicts['app_specific_init'] = chkpt_dicts['app_specific_init']
+        user_dicts['app_specific'] = chkpt_dicts['app_specific']
+        model.params(user_dicts['optz_sched'], user_dicts['app_specific'])
+    elif 'resume_from_checkpoint' in user_dicts[
+            'ld_resume_chkpt'] and user_dicts['ld_resume_chkpt'][
                 'resume_from_checkpoint'] is not None:
-        if 'resume_from_checkpoint' in user_params['trainer']:
+        if 'resume_from_checkpoint' in user_dicts['trainer']:
             strng = (f'Remove "resume_from_checkpoint" from the "trainer" '
                      f'dictionary in the file {argv[1]}.')
             logg.critical(strng)
             exit()
-        dirPath = Path(user_params['ld_resume_chkpt']
+        dirPath = Path(user_dicts['ld_resume_chkpt']
                        ['resume_from_checkpoint']).parents[1]
-        chkpt_params = full_load(
+        chkpt_dicts = full_load(
             dirPath.joinpath('hyperparameters_used.yaml').read_text())
-        # chkpt_params has 2 additional dicts - app_specific_init, app_specific
-        assert len(user_params) == len(chkpt_params) - 2
+        # chkpt_dicts has 2 additional dicts - app_specific_init, app_specific
+        assert len(user_dicts) == len(chkpt_dicts) - 2
 
-        # override  some user_params with chkpt_params
-        for (user_param_k,
-             user_param_v), (chkpt_param_k,
-                             chkpt_param_v) in zip(user_params.items(),
-                                                   chkpt_params.items()):
-            assert user_param_k == chkpt_param_k
-            if not user_param_v or user_param_k == 'model_init' or\
-                    user_param_k == 'optz_sched':
-                user_params[user_param_k] = chkpt_param_v
+        # override  some user_dicts with chkpt_dicts
         for user_dict_k in user_dicts_keys:
-            if not user_params[user_dict_k] or user_dict_k == 'model_init' or\
+            if (not user_dicts[user_dict_k] and
+                    user_dict_k != 'ld_resume_chkpt') or\
+                    user_dict_k == 'model_init' or\
                     user_dict_k == 'optz_sched':
-                user_params[user_dict_k] = chkpt_params[user_dict_k]
-        user_params['app_specific_init'] = chkpt_params['app_specific_init']
-        user_params['app_specific'] = chkpt_params['app_specific']
-        _ = user_params['trainer'].pop('resume_from_checkpoint', None)
-        user_params['trainer']['resume_from_checkpoint'] = user_params[
+                user_dicts[user_dict_k] = chkpt_dicts[user_dict_k]
+        user_dicts['app_specific_init'] = chkpt_dicts['app_specific_init']
+        user_dicts['app_specific'] = chkpt_dicts['app_specific']
+        _ = user_dicts['trainer'].pop('resume_from_checkpoint', None)
+        user_dicts['trainer']['resume_from_checkpoint'] = user_dicts[
             'ld_resume_chkpt']['resume_from_checkpoint']
 
-        model = Model(user_params['model_init'],
-                      user_params['app_specific_init'])
-        model.params(user_params['optz_sched'], user_params['app_specific'])
+        model = Model(user_dicts['model_init'],
+                      user_dicts['app_specific_init'])
+        model.params(user_dicts['optz_sched'], user_dicts['app_specific'])
     else:
         app_specific_init = {}  # parameters for initializing Model class
         app_specific = {}  # parameters needed throughout Model class
         app_specific_init['num_classes'] = 8
         app_specific['num_classes'] = 8
-        user_params['app_specific_init'] = app_specific_init
-        user_params['app_specific'] = app_specific
-        model = Model(user_params['model_init'],
-                      user_params['app_specific_init'])
-        model.params(user_params['optz_sched'], user_params['app_specific'])
+        user_dicts['app_specific_init'] = app_specific_init
+        user_dicts['app_specific'] = app_specific
+        model = Model(user_dicts['model_init'],
+                      user_dicts['app_specific_init'])
+        model.params(user_dicts['optz_sched'], user_dicts['app_specific'])
         tb_subDir = ",".join([
-            f'{item}={user_params["model_init"][item]}'
+            f'{item}={user_dicts["model_init"][item]}'
             for item in ['model_type', 'tokenizer_type']
-            if item in user_params['model_init']
+            if item in user_dicts['model_init']
         ])
         dirPath = Path('tensorboard_logs').joinpath(tb_subDir)
         dirPath.mkdir(parents=True, exist_ok=True)
@@ -128,29 +124,29 @@ def main():
     dirPath.mkdir(parents=True, exist_ok=True)
     paramFile = dirPath.joinpath('hyperparameters_used.yaml')
     paramFile.touch()
-    paramFile.write_text(dump(user_params))
+    paramFile.write_text(dump(user_dicts))
 
     # setup Callbacks and Trainer
-    if not ('no_training' in user_params['misc']
-            and user_params['misc']['no_training']):
+    if not ('no_training' in user_dicts['misc']
+            and user_dicts['misc']['no_training']):
         # Training: True, Testing: Don't care
         ckpt_filename = ""
-        if 'batch_size' in user_params['data']:
-            ckpt_filename += f'batch={user_params["data"]["batch_size"]},'
-        for item in user_params['optz_sched']:
-            if isinstance(user_params['optz_sched'][item], str):
-                ckpt_filename += f'{item}={user_params["optz_sched"][item]},'
-            elif isinstance(user_params['optz_sched'][item],
+        if 'batch_size' in user_dicts['data']:
+            ckpt_filename += f'batch={user_dicts["data"]["batch_size"]},'
+        for item in user_dicts['optz_sched']:
+            if isinstance(user_dicts['optz_sched'][item], str):
+                ckpt_filename += f'{item}={user_dicts["optz_sched"][item]},'
+            elif isinstance(user_dicts['optz_sched'][item],
                             collections.abc.Iterable):
-                for k, v in user_params['optz_sched'][item].items():
+                for k, v in user_dicts['optz_sched'][item].items():
                     ckpt_filename += f'{k}={v},'
         ckpt_filename += '{epoch:02d}-{val_loss:.5f}'
 
         checkpoint_callback = ModelCheckpoint(
             monitor='val_loss',
             mode='min',
-            save_top_k=user_params['misc']['save_top_k']
-            if 'save_top_k' in user_params['misc'] else 1,
+            save_top_k=user_dicts['misc']['save_top_k']
+            if 'save_top_k' in user_dicts['misc'] else 1,
             save_last=True,
             every_n_epochs=1,
             filename=ckpt_filename)
@@ -161,13 +157,13 @@ def main():
                           num_sanity_val_steps=0,
                           log_every_n_steps=100,
                           callbacks=[checkpoint_callback, lr_monitor],
-                          **user_params['trainer'])
-    elif not ('no_testing' in user_params['misc']
-              and user_params['misc']['no_testing']):
+                          **user_dicts['trainer'])
+    elif not ('no_testing' in user_dicts['misc']
+              and user_dicts['misc']['no_testing']):
         # Training: False, Testing: True
         trainer = Trainer(logger=True,
                           checkpoint_callback=False,
-                          **user_params['trainer'])
+                          **user_dicts['trainer'])
     else:
         # Training: False, Testing: False
         strng = ('User specified no-training and no-testing. Must do either'
@@ -175,30 +171,30 @@ def main():
         logg.critical(strng)
         exit()
 
-    data = Data(user_params['data'])
-    data.prepare_data(no_training=True if 'no_training' in user_params['misc']
-                      and user_params['misc']['no_training'] else False,
-                      no_testing=True if 'no_testing' in user_params['misc']
-                      and user_params['misc']['no_testing'] else False)
+    data = Data(user_dicts['data'])
+    data.prepare_data(no_training=True if 'no_training' in user_dicts['misc']
+                      and user_dicts['misc']['no_training'] else False,
+                      no_testing=True if 'no_testing' in user_dicts['misc']
+                      and user_dicts['misc']['no_testing'] else False)
     dataset_metadata = data.get_dataset_metadata()
     data.put_tokenizer(tokenizer=model.get_tokenizer())
 
     trainer.tune(model, datamodule=data)
-    if not ('no_training' in user_params['misc']
-            and user_params['misc']['no_training']):
+    if not ('no_training' in user_dicts['misc']
+            and user_dicts['misc']['no_training']):
         # Training: True, Testing: Don't care
         trainer.fit(model, datamodule=data)
-        if not ('no_testing' in user_params['misc']
-                and user_params['misc']['no_testing']):
-            if 'statistics' in user_params['misc'] and user_params['misc'][
+        if not ('no_testing' in user_dicts['misc']
+                and user_dicts['misc']['no_testing']):
+            if 'statistics' in user_dicts['misc'] and user_dicts['misc'][
                     'statistics']:
                 model.set_statistics(dataset_metadata)
             trainer.test()  # auto loads checkpoint file with lowest val loss
             model.clear_statistics()
-    elif not ('no_testing' in user_params['misc']
-              and user_params['misc']['no_testing']):
+    elif not ('no_testing' in user_dicts['misc']
+              and user_dicts['misc']['no_testing']):
         # Training: False, Testing: True
-        if 'statistics' in user_params['misc'] and user_params['misc'][
+        if 'statistics' in user_dicts['misc'] and user_dicts['misc'][
                 'statistics']:
             model.set_statistics(dataset_metadata)
         trainer.test(model, test_dataloaders=data.test_dataloader())
